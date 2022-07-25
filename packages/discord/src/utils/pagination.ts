0a1b6c77a76baf76e25830fa040ca01311f33a47
Amin <amin.haddou@frg.wwschool.de>
2022-07-25 12:13:48 +0200

    refactor redundant code in pagination.ts

--- packages/discord/src/utils/pagination.ts
+++ packages/discord/src/utils/pagination.ts
@@ -32,9 +32,7 @@ export class Pagination {
   async start() {
     const message = await this.interaction.reply({
       embeds: [this.pages[this.index]],
-      components: [
-        buildRow(this.index == 0, this.index == this.pages.length - 1),
-      ],
+      components: [this.row()],
       ephemeral: this.ephermal,
     });
 
@@ -77,39 +75,39 @@ export class Pagination {
 
       await interaction.update({
         embeds: [this.pages[this.index]],
-        components: [
-          buildRow(this.index == 0, this.index == this.pages.length - 1),
-        ],
+        components: [this.row()],
       });
     });
   }
-}
 
-const buildRow = (previousDisabled: boolean, nextDisabled: boolean) => {
-  return new ActionRowBuilder<ButtonBuilder>().addComponents([
-    new ButtonBuilder()
-      .setCustomId("0")
-      .setLabel("⏮️")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(previousDisabled),
-    new ButtonBuilder()
-      .setCustomId("1")
-      .setLabel("◀️")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(previousDisabled),
-    new ButtonBuilder()
-      .setCustomId("2")
-      .setLabel("⏹️")
-      .setStyle(ButtonStyle.Danger),
-    new ButtonBuilder()
-      .setCustomId("3")
-      .setLabel("▶️")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(nextDisabled),
-    new ButtonBuilder()
-      .setCustomId("4")
-      .setLabel("⏭️")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(nextDisabled),
-  ]);
-};
+  row() {
+    const previousDisabled = this.index <= 0;
+    const nextDisabled = this.index >= this.pages.length - 1;
+    return new ActionRowBuilder<ButtonBuilder>().addComponents([
+      new ButtonBuilder()
+        .setCustomId("0")
+        .setLabel("⏮️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(previousDisabled),
+      new ButtonBuilder()
+        .setCustomId("1")
+        .setLabel("◀️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(previousDisabled),
+      new ButtonBuilder()
+        .setCustomId("2")
+        .setLabel("⏹️")
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId("3")
+        .setLabel("▶️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(nextDisabled),
+      new ButtonBuilder()
+        .setCustomId("4")
+        .setLabel("⏭️")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(nextDisabled),
+    ]);
+  }
+}
