86c914992eeb73b9a643ab6dfda5b03b32ef309b
Amin <amin.haddou@frg.wwschool.de>
2022-07-27 10:01:10 +0200

    add presence

--- packages/discord/src/core/updater.ts
+++ packages/discord/src/core/updater.ts
@@ -1,11 +1,23 @@
-import { APIGuildMember, Routes } from "discord-api-types/v10";
+import { ActivityType, APIGuildMember, Routes } from "discord-api-types/v10";
 import { rest } from "../utils/rest";
 import { GUILD_ID } from "@avatar-history/env";
 import { updateAvatars } from "./avatars";
 import { updateUsernames } from "./usernames";
+import { client } from "./client";
+import { getAvatars } from "@avatar-history/db";
 
 export const update = async (limit: number) => {
   const members = await fetchMembers(limit);
+  const avatars = await getAvatars();
+  client.user!.setPresence({
+    status: "online",
+    activities: [
+      {
+        type: ActivityType.Watching,
+        name: `${members.length} Users and ${avatars.length} Avatars`,
+      },
+    ],
+  });
   await updateAvatars(members);
   await updateUsernames(members);
 };
