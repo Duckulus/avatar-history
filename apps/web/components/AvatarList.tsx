a79998bb3370d45ffdd8077496b743aceb5f9bf3
Amin <amin.haddou@frg.wwschool.de>
2022-07-28 11:38:44 +0200

    edit avatar list layout

--- apps/web/components/AvatarList.tsx
+++ apps/web/components/AvatarList.tsx
@@ -3,19 +3,25 @@ import Image from "next/image";
 
 export function AvatarList({ avatars }: { avatars: Avatar[] }) {
   return (
-    <ul className={"no-dot"}>
+    <div className={"avatar-list"}>
       {avatars.map((avatar, index) => {
-        return (
-          <li key={index}>
-            <Image
-              src={`https://cdn.duckul.us/avatars/${avatar.userId}/${avatar.id}.png`}
-              alt={"avatar"}
-              width={100}
-              height={100}
-            />
-          </li>
-        );
+        return <Avatar avatar={avatar} index={index} />;
       })}
-    </ul>
+    </div>
+  );
+}
+
+function Avatar({ avatar, index }: { avatar: Avatar; index: number }) {
+  const url = `https://cdn.duckul.us/avatars/${avatar.userId}/${avatar.id}.png`;
+  return (
+    <div className={"avatar"}>
+      <a target={"_blank"} href={url}>
+        Avatar no. {index + 1}
+      </a>
+      <br />
+      <a>{new Date(avatar.createdAt).toDateString()}</a>
+      <br />
+      <Image src={url} alt={"avatar"} width={100} height={100} />
+    </div>
   );
 }
