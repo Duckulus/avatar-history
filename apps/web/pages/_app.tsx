2bc131eac2fef5c47796c3ad48a136f135a9cb6c
Amin <amin.haddou@frg.wwschool.de>
2022-07-29 12:42:33 +0200

    edit favicon and title

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <title>Avatar History</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
